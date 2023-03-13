1f0d82915eb9aab4cf328ab69f02e5312a7a4bce
physcrowley <53307335+physcrowley@users.noreply.github.com>
2023-03-12 21:09:03 -0400

    removed system screen size reference and added random col

--- random_punch_cue.py
+++ random_punch_cue.py
@@ -1,11 +1,5 @@
 import pygame
-import Xlib.display
-
-# Get the system screen resolution
-display = Xlib.display.Display()
-screen = display.screen()
-SCREEN_WIDTH = screen.width_in_pixels
-SCREEN_HEIGHT = screen.height_in_pixels
+import random
 
 # Initialize Pygame
 pygame.init()
@@ -31,50 +25,40 @@ game_state = START_STATE
 pygame.display.set_caption("Pygame Example")
 
 # Create the window
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 1800
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Define the Rect objects for the buttons
 start_button = pygame.Rect(SCREEN_WIDTH/4 - 50, SCREEN_HEIGHT/2, 100, 50)
 quit_button = pygame.Rect(3*SCREEN_WIDTH/4 - 50, SCREEN_HEIGHT/2, 100, 50)
 
-# Define the functions
+# Define the Rect objects for the squares
+square_width = SCREEN_WIDTH/2
+square_height = SCREEN_HEIGHT/3
+squares = []
 
-def draw_start_screen():
-    # Clear the screen
-    window.fill(WHITE)
+for i in range(2):
+    for j in range(3):
+        square = pygame.Rect(i*square_width, j*square_height, square_width, square_height)
+        if (i+j) % 2 == 0:
+            squares.append((square, GRAY))
+        else:
+            squares.append((square, BLACK))
 
-    # Draw the title
-    title_text = FONT.render("Welcome to Pygame Example", True, BLACK)
-    title_rect = title_text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/4))
-    window.blit(title_text, title_rect)
+# Define the function to randomly change the color of a square
+def change_color():
+    for i in range(8):
+        square_index = random.randint(0, len(squares)-1)
+        square, color = squares[square_index]
+        squares[square_index] = (square, RED)
+        pygame.time.wait(4000)
+        squares[square_index] = (square, color)
+        pygame.time.wait(4000)
 
-    # Draw the start button
-    pygame.draw.rect(window, GREEN, start_button)
-    start_text = FONT.render("Start", True, WHITE)
-    start_rect = start_text.get_rect(center=start_button.center)
-    window.blit(start_text, start_rect)
-
-    # Draw the quit button
-    pygame.draw.rect(window, RED, quit_button)
-    quit_text = FONT.render("Quit", True, WHITE)
-    quit_rect = quit_text.get_rect(center=quit_button.center)
-    window.blit(quit_text, quit_rect)
-
-def draw_game_screen():
-    # Clear the screen
-    window.fill(WHITE)
-
-    # Draw the squares
-    square_width = SCREEN_WIDTH/2
-    square_height = SCREEN_HEIGHT/3
-
-    for i in range(2):
-        for j in range(3):
-            square = pygame.Rect(i*square_width, j*square_height, square_width, square_height)
-            if (i+j) % 2 == 0:
-                pygame.draw.rect(window, BLACK, square)
-            else:
-                pygame.draw.rect(window, GRAY, square)
+    # Change the game state back to START_STATE
+    global game_state
+    game_state = START_STATE
 
 # Start the game loop
 while True:
@@ -93,9 +77,29 @@ while True:
 
     # Draw the appropriate screen
     if game_state == START_STATE:
-        draw_start_screen()
-    elif game_state == GAME_STATE:
-        draw_game_screen()
+        # Clear the screen
+        window.fill(WHITE)
 
-    # Update the display
-    pygame.display.update()
+        # Draw the title
+        title_text = FONT.render("Welcome to Pygame Example", True, BLACK)
+        title_rect = title_text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/4))
+        window.blit(title_text, title_rect)
+
+        # Draw the start button
+        pygame.draw.rect(window, GREEN, start_button)
+        start_text = FONT.render("Start", True, WHITE)
+        start_rect = start_text.get_rect(center=start_button.center)
+        window.blit(start_text, start_rect)
+
+        # Draw the quit button
+        pygame.draw.rect(window, RED, quit_button)
+        quit_text = FONT.render("Quit", True, WHITE)
+        quit_rect = quit_text.get_rect(center=quit_button.center)
+        window.blit(quit_text, quit_rect)
+
+    elif game_state == GAME_STATE:
+        # Clear the screen
+        window.fill(WHITE)
+
+        # Draw the squares
+        for square, color in squares:
